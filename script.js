2e5adf32ea8d1d297b158756bef8a1cbf5ac268e
Mjareks <77351490+Death-Warrior@users.noreply.github.com>
2022-01-18 11:52:05 +0000

    pirms nodošnas (stundas sākums)

---
+++ script.js
@@ -0,0 +1,12 @@
+function kasir() {
+  window.open("https://minecraft-fan-website.jurgo4509.repl.co/Kas%20ir%20MC/kasirmc%20Dmitrijs.html", "_self");
+}
+function vesture() {
+  window.open("https://minecraft-fan-website.jurgo4509.repl.co/Vesture/vesture%20Mareks.html", "_self");
+}
+function fakti() {
+  window.open("https://minecraft-fan-website.jurgo4509.repl.co/Fakti/fakti.html", "_self");
+}
+function parmums() {
+  window.open("https://minecraft-fan-website.jurgo4509.repl.co/Par_Mums/parmums.html", "_self");
+}
